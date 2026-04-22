e0ebe8dac17267d46276accb2bf1d22b2b391973
Lina-hel <Lina.Helali@esprit.tn>
2026-04-22 11:39:44 +0100

    Refactor PanierService methods and update TestPanierCRUD for improved cart functionality

--- src/main/java/main/TestPanierCRUD.java
+++ src/main/java/main/TestPanierCRUD.java
@@ -9,12 +9,27 @@ public class TestPanierCRUD {
 
         try {
 
-            //panierService.addToCart(4, 3, 5);
-            //System.out.println("Produit ajouté au panier");
+            panierService.addToCart(4, 3, 2);
+            System.out.println("Produit ajouté !");
 
+            // 2) Afficher le panier
+            panierService.getCartByUser(4).forEach(item ->
+                    System.out.println("Produit ID: " + item[0] + " | Quantité: " + item[1])
+            );
 
-             panierService.removeFromCart(3);
-             System.out.println("Produit retiré du panier");
+            // 3) Modifier quantité
+            panierService.updateQuantite(4, 3, 5);
+            System.out.println("Quantité modifiée !");
+
+            // 4) Total
+            System.out.println("Total : " + panierService.getTotalPanier(4) + " TND");
+
+            // 5) Supprimer un produit
+            //panierService.removeFromCart(3);
+            //System.out.println("Produit retiré !");
+
+            // 6) Vider le panier
+            // panierService.clearCart(1);
 
         } catch (SQLException e) {
             e.printStackTrace();
